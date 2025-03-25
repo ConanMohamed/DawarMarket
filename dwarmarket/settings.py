1fcb502c3b54d08361580f3620235d4993ecd98d
omar01000 <aomar9452@gmail.com>
2025-03-25 21:06:47 +0200

    Fix: load static in baششِسيdfddddي

--- dwarmarket/settings.py
+++ dwarmarket/settings.py
@@ -63,7 +63,9 @@ ROOT_URLCONF = 'dwarmarket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'store', 'templates')
+],
+        
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
