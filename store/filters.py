7a4efee5a88a2c41704d4ca651b8b13ed2151423
omar01000 <aomar9452@gmail.com>
2025-04-10 13:36:20 +0200

    available store1

--- store/filters.py
+++ store/filters.py
@@ -2,30 +2,35 @@ import django_filters
 from .models import Product,Store,Category
 from django.db import models
 
+import django_filters
+from store.models import Product
+
 class ProductFilter(django_filters.FilterSet):
-    # فلترة السعر الأدنى
     min_price = django_filters.NumberFilter(field_name="unit_price", lookup_expr='gte')
-    # فلترة السعر الأعلى
     max_price = django_filters.NumberFilter(field_name="unit_price", lookup_expr='lte')
-    # البحث في العنوان باستخدام contains (غير حساس لحالة الأحرف)
     title = django_filters.CharFilter(field_name="title", lookup_expr='icontains')
-    # البحث باستخدام اسم المتجر
     store_name = django_filters.CharFilter(field_name="store__name", lookup_expr='icontains')
-    store = django_filters.NumberFilter(field_name="store__id")  # ✅ فلترة حسب المتجر بالـ ID
-    has_discount = django_filters.BooleanFilter(method='filter_has_discount',label='Has discount')
-    available = django_filters.BooleanFilter(field_name='available')
-    
-    
-    def filter_has_discount(self, queryset, name, value):
-        if value:
-            return queryset.filter(price_after_discount__lt=models.F('unit_price'))
-        return queryset
-    
-    
-    
+    store = django_filters.NumberFilter(field_name="store__id")
+    store_category = django_filters.NumberFilter(field_name="store_category__id")
+
+    has_discount = django_filters.BooleanFilter(method='filter_has_discount')
+    available = django_filters.BooleanFilter(method='filter_available')
+
     class Meta:
         model = Product
-        fields = ['title', 'store_name','store', 'min_price', 'max_price','store_category','has_discount','available']
+        fields = ['title', 'store_name', 'min_price', 'max_price', 'store', 'store_category', 'has_discount', 'available']
+
+    def filter_has_discount(self, queryset, name, value):
+        if value is True:
+            return queryset.filter(price_after_discount__lt=django_filters.F('unit_price'))
+        elif value is False:
+            return queryset.filter(price_after_discount=django_filters.F('unit_price'))
+        return queryset
+
+    def filter_available(self, queryset, name, value):
+        if isinstance(value, bool):
+            return queryset.filter(available=value)
+        return queryset
 
 
 
@@ -47,3 +52,8 @@ class CategoryFilter(django_filters.FilterSet):
     class Meta:
         model = Category
         fields = ['name']
+
+
+
+
+
