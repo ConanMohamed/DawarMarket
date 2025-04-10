902df0b6fc5f757cfaec983e95c3c1af311ee8e9
omar01000 <aomar9452@gmail.com>
2025-04-10 14:01:20 +0200

    test

--- store/filters.py
+++ store/filters.py
@@ -3,31 +3,12 @@ from .models import Product,Store,Category
 from django.db import models
 
 class ProductFilter(django_filters.FilterSet):
-    # فلترة السعر الأدنى
-    min_price = django_filters.NumberFilter(field_name="unit_price", lookup_expr='gte')
-    # فلترة السعر الأعلى
-    max_price = django_filters.NumberFilter(field_name="unit_price", lookup_expr='lte')
-    # البحث في العنوان باستخدام contains (غير حساس لحالة الأحرف)
-    title = django_filters.CharFilter(field_name="title", lookup_expr='icontains')
-    # البحث باستخدام اسم المتجر
-    store_name = django_filters.CharFilter(field_name="store__name", lookup_expr='icontains')
-    store = django_filters.NumberFilter(field_name="store__id")  # ✅ فلترة حسب المتجر بالـ ID
-    has_discount = django_filters.BooleanFilter(method='filter_has_discount',label='Has discount')
-    available = django_filters.BooleanFilter(field_name="available")  # ✅ أهو دا الجديد
-    
-    
-    
-    
-    def filter_has_discount(self, queryset, name, value):
-        if value:
-            return queryset.filter(price_after_discount__lt=models.F('unit_price'))
-        return queryset
-    
-    
-    
+    available = django_filters.BooleanFilter()
+
     class Meta:
         model = Product
-        fields = ['title', 'store_name','store', 'min_price', 'max_price','store_category','has_discount','available']
+        fields = ['available']
+
 
 
 
